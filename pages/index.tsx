0cdf22edad12898069faaef2c5a26553240d27d1
jbigman <jbigman@users.noreply.github.com>
2024-01-12 14:04:29 +0100

    updated title and alt for picture

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import Head from 'next/head'
 import Curiculum from './../src/curiculum/curiculum'
-import { Html } from 'next/document.js'
 
 const Index = () => {
   return (
-    <Html lang="en">
+    <>
       <Head>
         <title>Jérémie GAMBIN</title>
       </Head>
       <Curiculum />
-    </Html>
+    </>
   )
 }
 
