338f84894644814451952ca7c4b62cf92061c9f0
jbigman <jbigman@users.noreply.github.com>
2024-01-12 03:53:15 +0100

    fixed eslint

--- src/Components/links.tsx
+++ src/Components/links.tsx
@@ -1,15 +1,10 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiFillGithub, AiFillLinkedin, AiOutlineFileSearch } from 'react-icons/ai'
-import { FaStackOverflow } from 'react-icons/fa'
-
 const Links = () => {
   return (
     <div className="row center" style={{ columnGap: '25px' }}>
-      <a href="https://github.com/jbigman" target="_blank" title="github" rel="noreferrer"><AiFillGithub style={{ fontSize: '2em' }} /></a>
+      {/* <a href="https://github.com/jbigman" target="_blank" title="github" rel="noreferrer"><AiFillGithub style={{ fontSize: '2em' }} /></a>
       <a href="https://fr.linkedin.com/in/jeremiegambin" target="_blank" title="linkedin" rel="noreferrer"><AiFillLinkedin style={{ fontSize: '2em' }} /></a>
       <a href="http://stackoverflow.com/users/2988788/jbigman" target="_blank" title="stackoverflow" rel="noreferrer"><FaStackOverflow style={{ fontSize: '2em' }} /></a>
-      <Link href="/cv" target="_self" title="CV" rel="noreferrer"><AiOutlineFileSearch style={{ fontSize: '2em' }} /></Link>
+      <Link href="/cv" target="_self" title="CV" rel="noreferrer"><AiOutlineFileSearch style={{ fontSize: '2em' }} /></Link> */}
     </div>
   )
 }
